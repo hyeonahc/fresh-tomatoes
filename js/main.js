dcef88c30fbe6f95ac41bc149fb03d292ddb42d2
hyeonah <hyeonah.hello@gmail.com>
2022-05-07 09:02:58 +0900

    feat: Add axios and install regenerator runtime

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 import * as Popper from '@popperjs/core';
 import {} from '../node_modules/bootstrap';
+import 'regenerator-runtime';
+
+// async & await
+async function getMovie() {
+  const { data } = await axios({
+    url: 'https://www.omdbapi.com?apikey=7035c60c&s=frozen',
+    method: 'GET',
+  });
+  console.log({ data });
+}
+
+getMovie();
