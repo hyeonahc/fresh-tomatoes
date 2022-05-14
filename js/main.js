e94b681a66ae9cde03555537f3309a4b2c5d9e9d
hyeonah <hyeonah.hello@gmail.com>
2022-05-14 22:54:36 +0900

    feat: Create layout with styling and fetch movie data - create layout for searching and displaying movies - add reset.css, google fonts, favicon - fetch movie data when search button is clicked

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-import * as Popper from '@popperjs/core';
-import {} from '../node_modules/bootstrap';
 import 'regenerator-runtime';
 
-// async & await
-async function getMovie() {
-  const { data } = await axios({
-    url: 'https://www.omdbapi.com?apikey=7035c60c&s=frozen',
-    method: 'GET',
-  });
-  console.log({ data });
+async function getMovie(name, page = 1) {
+  let res = await fetch(
+    `https://www.omdbapi.com?apikey=7035c60c&s=${name}&page=1`
+  );
+  res = await res.json();
+  return res;
 }
 
-getMovie();
+let page = 1;
+let totalResults = 0;
+const searchEl = document.querySelector('.search input');
+const searchBtnEl = document.querySelector('.search .fa-magnifying-glass');
+
+searchBtnEl.addEventListener('click', async () => {
+  page = 1;
+  const movies = await getMovie(searchEl.value, page);
+  page = 2;
+  const { Search, totalResults } = movies;
+  const gridContainerEl = document.querySelector('.grid-container');
+
+  Search.forEach(movie => {
+    const movieNode = document
+      .querySelector('div.grid-item.hidden')
+      .cloneNode(true);
+    movieNode.classList.remove('hidden');
+    movieNode.querySelector('img').src = movie.Poster;
+    movieNode.querySelector(
+      'a'
+    ).href = `https://www.imdb.com/title/${movie.imdbID}/`;
+    movieNode.querySelector('.poster-title').innerHTML = movie.Title;
+    movieNode.querySelector('.poster-year').innerHTML = movie.Year;
+    gridContainerEl.append(movieNode);
+  });
+});
