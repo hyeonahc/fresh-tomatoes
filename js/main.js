51879b54fe76abf0ba1a04dc96afaa6cf94ced7e
hyeonah <hyeonah.hello@gmail.com>
2022-05-17 11:42:52 +0900

    feat: Create lazy load using intersection observer (INCOMPLETE)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import 'regenerator-runtime';
 
-async function getMovie(name, page) {
+const getMovie = async (name, page) => {
   let res = await fetch(
     `https://www.omdbapi.com?apikey=7035c60c&s=${name}&page=${page}`
   );
   res = await res.json();
   return res;
-}
+};
 
 let page = 1;
 let totalResults = 0;
@@ -14,18 +14,21 @@ const searchEl = document.querySelector('.search input');
 const searchBtnEl = document.querySelector('.search .fa-magnifying-glass');
 
 searchBtnEl.addEventListener('click', async () => {
-  document.querySelector('.welcome-page').classList.add('hidden');
-  document.querySelector('.movie-page').classList.remove('hidden');
-  page = 1;
-  const movies = await getMovie(searchEl.value, page);
-  console.log(movies);
-  page = 2;
+  if (page === 1) {
+    document.querySelector('.welcome-page').classList.add('hidden');
+    document.querySelector('.movie-page').classList.remove('hidden');
+  }
+  let movies = await getMovie(searchEl.value, page);
+  renderMovie(movies);
+});
+
+// async arrow function not working
+function renderMovie(movies) {
   const { Search, totalResults } = movies;
   const movieContainerEl = document.querySelector('.movie-container');
   console.log(Search, totalResults);
 
   Search.forEach(movie => {
-    console.log(movie);
     const movieNode = document
       .querySelector('div.movie-item.hidden')
       .cloneNode(true);
@@ -38,4 +41,36 @@ searchBtnEl.addEventListener('click', async () => {
     movieNode.querySelector('.movie-year').innerHTML = movie.Year;
     movieContainerEl.append(movieNode);
   });
-});
+  // page = 2;
+  loadMore(page);
+}
+
+/* Create lazy load using intersection observer (NOT WORKING)
+const loadMore = page => {
+  // const loadMoreEl = document.querySelector('.load-more');
+  const movieItemEls = document.querySelectorAll('.movie-item:not(.hidden)');
+
+  const movieItemObserver = new IntersectionObserver(entries => {
+    entries.forEach(
+      entry => {
+        console.log(entry);
+        const { isIntersecting } = entry;
+        console.log(isIntersecting);
+        if (isIntersecting) {
+          // page += 1;
+          // console.log('page', page);
+          // renderMovie(page)
+          // movieItemObserver.unobserve(entry.target);
+        }
+      },
+      {
+        threshold: 1,
+      }
+    );
+  });
+
+  movieItemEls.forEach(movieItemEl => {
+    movieItemObserver.observe(movieItemEl);
+  });
+};
+*/
